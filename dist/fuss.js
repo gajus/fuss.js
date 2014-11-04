9deb313f0dbdd1e7ae61d717bc047663f5c1821b
Gajus Kuizinas <gk@anuary.com>
2014-11-04 14:50:51 +0200

    Bower did not pick up the version bump.

--- dist/fuss.js
+++ dist/fuss.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.1.3
+ * @version 1.2.0
  * @link https://github.com/gajus/fuss for the canonical source repository
  * @license https://github.com/gajus/fuss/blob/master/LICENSE BSD 3-Clause
  */
