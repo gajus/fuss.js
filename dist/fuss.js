561ee8a5163cc0b1b6de66fdd8840e286cf17368
Gajus Kuizinas <gk@anuary.com>
2014-12-04 13:31:47 +0200

    Strict property checking.

--- dist/fuss.js
+++ dist/fuss.js
@@ -1,5 +1,5 @@
 /**
- * @version 1.2.4
+ * @version 1.2.5
  * @link https://github.com/gajus/fuss for the canonical source repository
  * @license https://github.com/gajus/fuss/blob/master/LICENSE BSD 3-Clause
  */
